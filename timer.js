eae1489f688dc43e1848f72cb8d1239432d3ee5b
Zeusyuh <49097720+Zeusyuh@users.noreply.github.com>
2019-04-01 21:40:03 +0800

    4.4.1 Format
    Improved format
    *label needs fix*

---
+++ timer.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date("Sep 1, 2019 00:00:00").getTime();
+
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countDown").innerHTML = days + " | " + hours + " | " + minutes + " | " + seconds;
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000); 
+// JavaScript Document
