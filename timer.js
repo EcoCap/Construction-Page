861d183c1010d71723f7c181e2aa6e82788ae81c
uumonki <atsutayusuke@outlook.com>
2019-04-02 21:30:46 +0800

    19.4.2 (3) Update
    NOTE: EXTREMELY BUGGY AND INCONSISTENT VERSION. MESSING AROUND COULD CAUSE SERIOUS PROBLEMS.
    
    New features: <progress/> bar.
    Resized elements, organized structure.
    
    Work in progress:
    * Font: preferably a different font for h1 and h2.
    * Progress bar. Needs to be centered.

--- timer.js
+++ timer.js
@@ -8,15 +8,19 @@ var x = setInterval(function() {
   var totDistance = countDownDate - startDate;
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  /*var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  var progressWidth = (1 - distance / totDistance) * 50 + "vw";
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);*/
+  var Progress = 0.8;//(1 - distance / totDistance);
 	
 
   // Display the result in the element with id="demo"
-  document.getElementById("countDown").innerHTML = days + " | " + hours + " | " + minutes + " | " + seconds;
-	document.getElementById("progress-bar").style.width = progressWidth;
+  document.getElementById("countDown").innerHTML = days + " days";
+  var i = 0.01;
+  while (i <= 1) {
+	  document.getElementById("progress").value = ease(i) * Progress;
+	  setTimeout(i += 0.01, 10);
+  }
 
   // If the count down is finished, write some text 
   if (distance < 0) {
@@ -24,4 +28,9 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000); 
+
+function ease(t) {
+	return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 
+}
+
 // JavaScript Document
