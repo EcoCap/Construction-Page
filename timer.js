2e0fbd80c6a8446cd3fadda5cc10e93412d53074
Zeusyuh <jasper7k7k@outlook.com>
2019-04-02 16:23:18 +0800

    19.4.2 (1) Update
    Added progress bar
    Yet to be implemented:
    *Font

--- timer.js
+++ timer.js
@@ -1,17 +1,23 @@
-var countDownDate = new Date("Sep 1, 2019 00:00:00").getTime();
+var countDownDate = new Date("Apr 3, 2019 00:00:00").getTime();
+var startDate = new Date("Apr 1, 2019 00:00:00");
+var progressWidth = 0;
 
 
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
+  var totDistance = countDownDate - startDate;
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  progressWidth = (1 - distance / totDistance) * 50 + "vw";
+	
 
   // Display the result in the element with id="demo"
   document.getElementById("countDown").innerHTML = days + " | " + hours + " | " + minutes + " | " + seconds;
+	document.getElementById("progress-bar").style.width = progressWidth;
 
   // If the count down is finished, write some text 
   if (distance < 0) {
